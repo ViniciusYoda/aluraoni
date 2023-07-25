a866e6a4724d30d5de0aad69c1fab41b9e26364b
Vinícius Yoda <vyoda4604@gmail.com>
2023-07-25 17:17:37 -0300

    ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "normalize.css";
 import './index.css';
+import Cardapio from './pages/Cardapio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Cardapio />
   </React.StrictMode>
 );
 
