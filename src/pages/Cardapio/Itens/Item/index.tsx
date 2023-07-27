4a6ab55850ad608a5b94b282ca6382a11a7830c9
Vinícius Yoda <vyoda4604@gmail.com>
2023-07-27 13:42:04 -0300

    tsx

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,31 +1,23 @@
-import styles from './Item.module.scss'; 
-import cardapio from 'data/cardapio.json';
-import classNames from 'classnames';
+import styles from './Item.module.scss';
+import { Prato } from 'types/Prato';
+import TagsPrato from 'components/TagsPrato';
+import { useNavigate } from 'react-router-dom';
 
-type Props = typeof cardapio[0];
-
-export default function Item(props: Props) {
-    const { title, description, category, size, serving, price, photo } = props;
-    return (
-        <div className={styles.item}>
-            <div className={styles.item__imagem}>
-                <img src={photo} alt={title} />
-            </div>
-            <div className={styles.item__descricao}>
-                <div className={styles.item__titulo}>
-                    <h2> {title} </h2>
-                    <p> {description} </p>
-                </div>
-                <div className={styles.item__tags}>
-                    <div className={classNames({
-                        [styles.item__tipo]: true,
-                        [styles[`item__tipo__${category.label.toLowerCase()}`]]: true
-                    })}>{category.label}</div>
-                    <div className={styles.item__porcao}>{size}g</div>
-                    <div className={styles.item__qtdpessoas}>{serving} 2 pessoa{serving === 1 ? '' : 's'}</div>
-                    <div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
-                </div>
-            </div>
+export default function Item(props: Prato) {
+  const { id, title, description, photo } = props;
+  const navigate = useNavigate();
+  return (
+    <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
+      <div className={styles.item__imagem}>
+        <img src={photo} alt={title} />
+      </div>
+      <div className={styles.item__descricao}>
+        <div className={styles.item__titulo}>
+          <h2> {title} </h2>
+          <p> {description} </p>
         </div>
-    );
+        <TagsPrato {...props} />
+      </div>
+    </div>
+  );
 }
